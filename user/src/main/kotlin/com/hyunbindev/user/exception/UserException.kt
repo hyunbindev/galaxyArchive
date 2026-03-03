59595cd255cea765e0b082806822dc39fdb486db
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-03-03 21:41:48 +0900

    user exception handling

---
+++ user/src/main/kotlin/com/hyunbindev/user/exception/UserException.kt
@@ -0,0 +1,4 @@
+package com.hyunbindev.user.exception
+
+class UserException {
+}
---
+++ user/src/main/kotlin/com/hyunbindev/user/exception/constant/UserExceptionCode.kt
@@ -0,0 +1,6 @@
+package com.hyunbindev.user.exception.constant
+
+import com.hyunbindev.common.exception.constant.ExceptionCode
+
+enum class UserExceptionCode : ExceptionCode {
+}
